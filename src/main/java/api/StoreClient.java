da34ebfe948df4fe20d2f6c66f666aec03277210
dasharomanova <romanovadaria.d@gmail.com>
2026-06-25 16:36:44 +0300

    Add getOrder, getOrderRaw and deleteOrderRaw methods to StoreClient

--- src/main/java/api/StoreClient.java
+++ src/main/java/api/StoreClient.java
@@ -7,21 +7,21 @@ import config.RequestSpec;
 
 public class StoreClient {
 
-    public Response getInventory(){
+    public Response getInventory() {
         return given()
                 .spec(RequestSpec.requestSpec)
                 .when()
                 .get("/store/inventory");
     }
 
-    public Order createOrder(Order order){
+    public Order createOrder(Order order) {
         Response response = createOrderRaw(order);
         response.then().statusCode(200);
         Order createOrder = response.as(Order.class);
         return createOrder;
     }
 
-    public Response createOrderRaw(Order order){
+    public Response createOrderRaw(Order order) {
         return given()
                 .spec(RequestSpec.requestSpec)
                 .body(order)
@@ -29,21 +29,21 @@ public class StoreClient {
                 .post("/store/order/");
     }
 
-    public Response getRawOrder(long orderID){
+    public Response getRawOrder(long orderID) {
         return given()
                 .spec(RequestSpec.requestSpec)
                 .when()
                 .get("/store/order/" + orderID);
     }
 
-    public  Order getOrder(long orderID){
+    public Order getOrder(long orderID) {
         Response response = getRawOrder(orderID);
         response.then().statusCode(200);
         Order order = response.as(Order.class);
         return order;
     }
 
-    public Response deleteOrderRaw(long orderID){
+    public Response deleteOrderRaw(long orderID) {
         return given()
                 .spec(RequestSpec.requestSpec)
                 .when()
